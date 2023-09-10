aeb9419962c18b27534a6057a4900d90f5f4b70a
Vera Baldina~ <vera.b97@mail.ru>
2023-09-10 11:36:33 +0300

    fix: исправить проверку id у карточки

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -3,7 +3,7 @@ import { CurrentUserContext } from "../contexts/CurrentUserContext";
 
 export default function Card(props) {
   const currentUser = useContext(CurrentUserContext);
-  const isOwn = (props.card.owner._id || props.card.owner) === currentUser._id;
+  const isOwn = props.card.owner === currentUser._id;
   const isLiked = props.card.likes.some((i) => i._id === currentUser._id);
   const cardLikeButtonClassName = `elements__like-button button ${
     isLiked && "elements__like-button_active"
